4804ccdb245678f2d92e8adaf9cded36f3dd7836
Peter Wildfeuer <wildfeuer.peter@gmail.com>
2018-01-19 21:45:42 -0500

    Added Page Turning Markers

--- Content/booklet.js
+++ Content/booklet.js
@@ -1,52 +1,58 @@
 var current = 0;
 
-function turn(prefix,num){
-
+function getPages(prefix,num,mPrefix){
     var list = [];
+    var marks = [];
 
     for(var i=0; i<num; i++){
         var n = i+1;
         var name = prefix + n.toString();
-        list.push(name)
+        var mark = mPrefix + n.toString();
+        list.push(name);
+        marks.push(mark);
     }
 
     console.log(list);
 
     var divs = [];
+    var markers = [];
     for(var i=0; i<list.length;i++){
         divs.push(document.getElementById(list[i]));
+        markers.push(document.getElementById(marks[i]))
     }
+    return [divs,markers];
+}
 
-    if(current<list.length-1) {
+function turn(prefix,num,mPrefix){
+
+    var all = getPages(prefix,num,mPrefix);
+    var divs = all[0];
+    var markers = all[1];
+
+    if(current<divs.length-1) {
         current = current + 1;
     }else{
-        current = list.length-1;
+        current = divs.length-1;
     }
 
     for(var i=0; i<divs.length; i++){
         if(i == current){
             divs[i].style.opacity=1;
+            markers[i].style.opacity = .75;
         }else{
             divs[i].style.opacity = 0;
+            markers[i].style.opacity = .1;
         }
     }
 }
 
 
-function turnBack(prefix,num){
+function turnBack(prefix,num,mPrefix){
 
-    var list = [];
+    var all = getPages(prefix,num,mPrefix);
+    var divs = all[0];
+    var markers = all[1];
 
-    for(var i=0; i<num; i++){
-        var n = i+1;
-        var name = prefix+ n.toString();
-        list.push(name)
-    }
-
-    var divs = [];
-    for(var i=0; i<list.length;i++){
-        divs.push(document.getElementById(list[i]));
-    }
     if(current>0) {
         current = current - 1;
     }else{
@@ -56,8 +62,10 @@ function turnBack(prefix,num){
     for(var i=0; i<divs.length; i++){
         if(i == current){
             divs[i].style.opacity = 1;
+            markers[i].style.opacity = .75;
         }else{
             divs[i].style.opacity = 0;
+            markers[i].style.opacity = .1;
         }
     }
 }
